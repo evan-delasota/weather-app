48f46ce0ef837eff2f2c19bce54be3781ee9b291
Evan D <evan.delasota@gmail.com>
2021-05-08 02:04:44 -0700

    destructured app.js

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const address = process.argv[2];
+
 if (!address) {
     console.log("Please provide an address");
 } else {
-    geocode(address, (error, geoData) => {
+    geocode(address, (error, { latitude, longitude, name } = {}) => {
         if (error) {
             return console.log(error);
         }
         
-        forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error);
             }
     
-            console.log("Current weather for " + geoData.name);
+            console.log("Current weather for " + name);
             console.log(forecastData);
         })
     });
