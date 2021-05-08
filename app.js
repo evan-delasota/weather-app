cb042f6f680ae5178fe28b5e0bb9a484fddea8f0
Evan D <evan.delasota@gmail.com>
2021-05-08 01:45:44 -0700

    Added simple error handling

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const address = process.argv[2];
-
-geocode(address, (error, geoData) => {
-    if (error) {
-        return console.log(error);
-    }
-    
-    forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+if (!address) {
+    console.log("Please provide an address");
+} else {
+    geocode(address, (error, geoData) => {
         if (error) {
             return console.log(error);
         }
-
-        console.log("Current weather for " + geoData.name);
-        console.log(forecastData);
-    })
-});
+        
+        forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+    
+            console.log("Current weather for " + geoData.name);
+            console.log(forecastData);
+        })
+    });
+}
