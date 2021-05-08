f19250894872ad2f9edaa0870fea28f02786fd79
Evan D <evan.delasota@gmail.com>
2021-05-08 02:06:29 -0700

    destructured forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=9748c79b8c11290034e9e4f4d55b38c3&units=f&query=' + latitude + ',' + longitude;
     
-    request({ url:url, json:true}, (error, res) => {
+    request({ url:url, json:true}, (error, { body }) => {
         if (error) {
             callback("Cannot connect to weather service.", undefined);
-        } else if (res.body.error) {
+        } else if (body.error) {
             callback("Unable to find location.", undefined);
         } else {
-            callback(undefined, res.body.current.weather_descriptions[0] 
-                                + ", it's currently " + res.body.current.temperature + " degrees out.\n" 
-                                + "There's a "  + res.body.current.precip + "% chance of rain.");
+            callback(undefined, body.current.weather_descriptions[0] 
+                                + ", it's currently " + body.current.temperature + " degrees out.\n" 
+                                + "There's a "  + body.current.precip + "% chance of rain.");
         }
     });
 
