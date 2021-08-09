cc4d4761e4193452f0ae4843ebd0edad980bfbfc
Evan D <evan.delasota@gmail.com>
2021-08-08 19:28:20 -0700

    modified port logging string

--- src/app.js
+++ src/app.js
@@ -87,5 +87,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log("Server successfully started on port 3000");
+    console.log("Server successfully started on port " + port);
 });
